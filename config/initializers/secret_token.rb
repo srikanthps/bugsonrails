1596bf1b2b9b7851cd6143abeef9e360bba4ecc6
Srikanth Shreenivas <srikanthps@yahoo.com>
2012-06-30 22:47:36 +0530

    empty rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugsonrails::Application.config.secret_token = 'f3e413a3d2756f8a976dd51bf649649d12636dd78a2ec14f428299e6b2410544d02fd111fbdf70104d29a1d88744938e336d07b893326b1d13b81c0d48b0c938'
